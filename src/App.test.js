8cc49df2f9c487cbe3a23bb5ff3fb873ac8a0faf
Kapil Viren Ahuja <k.v.ahuja@gmail.com>
2017-01-31 10:35:55 +0530

    Fixed the test case (default one). Also added file for building with Travis

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const initialPlayers = [
+  {
+    id: 1,
+    name: "Kapil Viren Ahuja",
+    score: 25,
+  },
+  {
+    id: 2,
+    name: "Aabhya Ahuja",
+    score: 50,
+  },
+  {
+    id: 3,
+    name: "Abeer Ahuja",
+    score: 75,
+  },
+];
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  <App title="My Scoreboard" initialPlayers={initialPlayers}/>,
+  document.getElementById('container')
+
+  ReactDOM.render(<App title="My Scoreboard" initialPlayers={initialPlayers}/>, div);
 });
